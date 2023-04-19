445ab47c133a747ecbe32f0ae17f9700cb7e4b33
Igor Conde <igorgabrielconde@gmail.com>
2023-04-18 21:49:46 -0300

    refactor(CryptoUtils): Atualizado

--- src/core/utils/crypto.utils.ts
+++ src/core/utils/crypto.utils.ts
@@ -1,26 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { randomBytes, timingSafeEqual } from 'crypto';
-import { genSalt, hash } from 'bcrypt';
+import { genSalt, hash, compare as bcryptCompare } from 'bcrypt';
 
 @Injectable()
 export class CryptoUtils {
   async generateSalt(rounds = 10): Promise<string> {
-    return await genSalt(rounds);
+    return genSalt(rounds);
   }
 
   async hash(data: string, salt: string): Promise<string> {
-    return await hash(data, salt);
+    return hash(data, salt);
   }
 
-  async compare(data: string, hash: string): Promise<boolean> {
-    const dataHash = await this.hash(data, hash.slice(0, 29));
-    const bufferDataHash = Buffer.from(dataHash);
-    const bufferHash = Buffer.from(hash);
-
-    if (bufferDataHash.length !== bufferHash.length) {
-      return false;
-    }
-
-    return timingSafeEqual(bufferDataHash, bufferHash);
+  async compare(password: string, hashedPassword: string): Promise<boolean> {
+    return bcryptCompare(password, hashedPassword);
   }
 }
