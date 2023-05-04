42462b38c723f708f6617157176a5bb1fdb6527b
Igor Conde <igorgabrielconde@gmail.com>
2023-05-04 02:23:52 -0300

    feat: perfil Module

--- src/modules/perfil/perfil.module.ts
+++ src/modules/perfil/perfil.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PerfilController } from './controllers/perfil.controller';
+import { PerfilService } from './services/perfil.service';
 
-@Module({})
+@Module({
+  controllers: [PerfilController],
+  providers: [PerfilService]
+})
 export class PerfilModule {}
