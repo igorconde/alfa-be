c854a61d53360247e63f3d622a1cfd9f979005e4
Igor Conde <igorgabrielconde@gmail.com>
2023-08-30 22:49:28 -0300

    feat: init novo decorator

---
+++ src/core/decorators/permissions.decorator.ts
@@ -0,0 +1,12 @@
+import { SetMetadata } from '@nestjs/common';
+import { IPermissionSlug } from '@leaa/api/src/interfaces';
+
+export const PermissionsMetadataKey = 'permissions';
+
+// TIPS: Permissions Decorator default condition is `AND`
+//
+// e.g. `@Permissions('p-delete', 'a-read')`, MUST BOTH HAS 'p-delete' && 'a-read'
+//
+export const Permissions = (...permissions: IPermissionSlug[]) => {
+  return SetMetadata(PermissionsMetadataKey, permissions);
+};
