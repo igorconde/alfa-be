4e33b1ee50c8722346abdb9c3d54a7303534786e
Igor Conde <igorgabrielconde@gmail.com>
2023-08-30 22:27:37 -0300

    hotfix: melhorias no controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,73 +1,67 @@
-import { RegisterDto } from './dto/register.dto';
-import { Body, Controller, Get, InternalServerErrorException, Post, Req, Res, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res, UseGuards, InternalServerErrorException } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Request, Response } from 'express';
+
 import { AuthService } from './auth.service';
 import { LogInWithCredentialsGuard } from './guards/local-auth.guard';
-import { LoginAuthDto } from './dto/login-auth.dto';
-import { Request, Response } from 'express';
-import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { Usuario } from '../usuario/entities/usuario.entity';
 import { PublicRoute } from '../../core/decorators/public-route.decorator';
 
-@ApiTags('Authentication')
+import { RegisterDto } from './dto/register.dto';
+import { LoginAuthDto } from './dto/login-auth.dto';
+import { Usuario } from '../usuario/entities/usuario.entity';
+
+@ApiTags('Autenticação')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @ApiOperation({ summary: 'Autenticar usuário', description: 'Retorna o objeto de usuário autenticado' })
-  @ApiBearerAuth()
-  @ApiResponse({ status: 200, description: 'Objeto de usuário autenticado', type: Usuario })
   @Get('/me')
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Autenticar usuário', description: 'Retorna os dados do usuário autenticado.' })
+  @ApiResponse({ status: 200, description: 'Usuário autenticado com sucesso.', type: Usuario })
   async authenticate(@Req() request: any): Promise<any> {
     return { data: request.user };
   }
 
-  @ApiOperation({
-    summary: 'Login de usuário',
-    description: 'Faz login do usuário e retorna o objeto de usuário autenticado',
-  })
+  @Post('login')
   @PublicRoute()
   @UseGuards(LogInWithCredentialsGuard)
-  @ApiResponse({ status: 200, description: 'Objeto de usuário autenticado', type: Usuario })
-  @ApiResponse({ status: 401, description: 'Não autorizado' })
-  @Post('login')
+  @ApiOperation({
+    summary: 'Login de usuário',
+    description: 'Realiza o login e retorna os dados do usuário autenticado.',
+  })
+  @ApiResponse({ status: 200, description: 'Login realizado com sucesso.', type: Usuario })
+  @ApiResponse({ status: 401, description: 'Não autorizado.' })
   async login(@Body() loginDto: LoginAuthDto): Promise<Usuario> {
     const { email, password } = loginDto;
     return await this.authService.authenticate(email, password);
   }
 
-  @ApiOperation({ summary: 'Logout de usuário', description: 'Faz logout do usuário autenticado' })
-  @PublicRoute()
-  @ApiResponse({ status: 200, description: 'Logout bem-sucedido' })
-  @ApiResponse({ status: 500, description: 'Não foi possível fazer logout do usuário' })
   @Post('logout')
-  async logout(@Req() request: Request, @Res({ passthrough: true }) response: Response) {
+  @PublicRoute()
+  @ApiOperation({ summary: 'Logout de usuário', description: 'Desconecta o usuário atual.' })
+  @ApiResponse({ status: 200, description: 'Logout realizado com sucesso.' })
+  @ApiResponse({ status: 500, description: 'Erro interno ao realizar logout.' })
+  async logout(@Req() request: Request, @Res({ passthrough: true }) response: Response): Promise<any> {
     const logoutError = await new Promise<Error | undefined>((resolve) =>
       request.logOut({ keepSessionInfo: false }, (error) => resolve(error)),
     );
 
     if (logoutError) {
-      throw new InternalServerErrorException('Não foi possível fazer logout do usuário');
+      throw new InternalServerErrorException('Não foi possível realizar o logout.');
     }
 
-    response.clearCookie('connect.sid', {
-      path: '/',
-    });
-
-    return {
-      logout: true,
-    };
+    response.clearCookie('connect.sid', { path: '/' });
+    return { logout: true };
   }
 
-  @ApiOperation({
-    summary: 'Registro de usuário',
-    description: 'Registra um novo usuário e retorna o objeto de usuário registrado',
-  })
-  @PublicRoute()
-  @ApiResponse({ status: 201, description: 'Objeto de usuário registrado', type: Usuario })
-  @ApiResponse({ status: 400, description: 'Solicitação inválida' })
-  @ApiResponse({ status: 409, description: 'Conflito' })
   @Post('register')
-  async register(@Body() registrationData: Readonly<RegisterDto>): Promise<Usuario> {
+  @PublicRoute()
+  @ApiOperation({ summary: 'Registro de usuário', description: 'Cadastra um novo usuário e retorna seus dados.' })
+  @ApiResponse({ status: 201, description: 'Usuário registrado com sucesso.', type: Usuario })
+  @ApiResponse({ status: 400, description: 'Dados de registro inválidos.' })
+  @ApiResponse({ status: 409, description: 'E-mail já cadastrado.' })
+  async register(@Body() registrationData: RegisterDto): Promise<Usuario> {
     return await this.authService.registerUser(registrationData);
   }
 }
