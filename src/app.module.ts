2db32f4a7df13f52821ca3de83f1808a61baa408
Igor Conde <igorgabrielconde@gmail.com>
2023-09-10 00:45:35 -0300

    feat: absolute path

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsuarioModule } from './modules/usuario/usuario.module';
-import databaseConf from './core/config/database.conf';
-import redisConf from './core/config/redis.conf';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TransformInterceptor } from './core/interceptors/transform.interceptor';
 import { PassportModule } from '@nestjs/passport';
 import { APP_GUARD } from '@nestjs/core';
-import { AuthModule } from './modules/auth/auth.module';
 import { PublicRoutesGuard } from './modules/auth/guards/public-routes.guard';
-import { PermissionModule } from './modules/permission/permission.module';
-import { RoleModule } from './modules/role/role.module';
-import appConf from './core/config/app.conf';
+import { AuthModule } from '@modules/auth/auth.module';
+import { UsuarioModule } from '@modules/usuario/usuario.module';
+import { PermissionModule } from '@modules/permission/permission.module';
+import { RoleModule } from '@modules/role/role.module';
+import databaseConf from '@config/database.conf';
+import redisConf from '@config/redis.conf';
+import appConf from '@config/app.conf';
 
 @Module({
   imports: [
