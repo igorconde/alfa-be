c5e2e1ed7e6fd9da517920f4a09bebd4f48e7de4
Igor Conde <igorgabrielconde@gmail.com>
2023-09-28 00:58:08 -0300

    feat(mail): criação do novo modulo de e-mail

---
+++ src/mail/mail-config.service.ts
@@ -0,0 +1,36 @@
+import { MailerOptions, MailerOptionsFactory } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as path from 'path';
+
+@Injectable()
+export class MailConfigService implements MailerOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
+  createMailerOptions(): MailerOptions {
+    return {
+      transport: {
+        host: this.configService.get('mail.host'),
+        port: this.configService.get('mail.port'),
+        secure: this.configService.get('mail.secure'),
+        requireTLS: this.configService.get('mail.requireTLS'),
+        ignoreTLS: this.configService.get('mail.ignoreTLS'),
+        auth: {
+          user: this.configService.get('mail.user'),
+          pass: this.configService.get('mail.password'),
+        },
+      },
+      defaults: {
+        from: `"${this.configService.get('mail.defaultName')}" <${this.configService.get('mail.defaultEmail')}>`,
+      },
+      template: {
+        dir: path.join(this.configService.get('app.workingDirectory'), 'src', 'mail', 'mail-templates'),
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    } as MailerOptions;
+  }
+}
