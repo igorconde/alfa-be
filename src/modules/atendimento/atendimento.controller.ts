6368726cf82030177381b76c86e087588793eac0
Igor Conde <igorgabrielconde@gmail.com>
2023-11-27 00:19:28 -0300

    Refactor findAll method in AtendimentoService to use pagination

--- src/modules/atendimento/atendimento.controller.ts
+++ src/modules/atendimento/atendimento.controller.ts
@@ -1,13 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+import { Paginate, PaginateQuery, Paginated } from 'nestjs-paginate';
 import { AtendimentoService } from './atendimento.service';
+import { Atendimento } from './entities/atendimento.entity';
 
 @Controller('atendimento')
 export class AtendimentoController {
   constructor(private readonly atendimentoService: AtendimentoService) {}
 
   @Get()
-  async exportarExcel() {
-    const resp = await this.atendimentoService.findAll();
+  async findAll(@Paginate() query: PaginateQuery): Promise<Paginated<Atendimento>> {
+    const resp = await this.atendimentoService.findAll(query);
 
     return resp;
   }
--- src/modules/atendimento/atendimento.service.ts
+++ src/modules/atendimento/atendimento.service.ts
@@ -1,6 +1,7 @@
 // seu-servico.service.ts
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { PaginateConfig, PaginateQuery, Paginated, paginate } from 'nestjs-paginate';
 import { Repository } from 'typeorm';
 import { Atendimento } from './entities/atendimento.entity';
 
@@ -11,21 +12,32 @@ export class AtendimentoService {
     private atendimentoRepository: Repository<Atendimento>,
   ) {}
 
-  async findAll(): Promise<Atendimento[]> {
-    return this.atendimentoRepository.find({
-      take: 5,
-      relations: [
-        'idAtendimentoSubetapa',
-        'idapl',
-        'idatendimentostatus2',
-        'idcliente2',
-        'idprodutolinha2',
-        'idprodutoregional2',
-        'idregional',
-        'idsolicitacaostatus',
-        'idunidade2',
-        'previsaoreceitas',
-      ],
-    });
+  async findAll(query: PaginateQuery): Promise<Paginated<Atendimento>> {
+    const paginateConfig: PaginateConfig<Atendimento> = {
+      defaultLimit: 20,
+      sortableColumns: ['id', 'titulo'],
+      nullSort: 'last',
+      defaultSortBy: [['id', 'ASC']],
+      searchableColumns: ['titulo', 'id'],
+      select: [],
+      filterableColumns: {},
+    };
+
+    const queryBuilder = this.atendimentoRepository
+      .createQueryBuilder('atendimento')
+      .leftJoinAndSelect('atendimento.idAtendimentoSubetapa', 'idAtendimentoSubetapa')
+      .leftJoinAndSelect('atendimento.idapl', 'idapl')
+      .leftJoinAndSelect('atendimento.idatendimentostatus2', 'idatendimentostatus2')
+      .leftJoinAndSelect('atendimento.idcliente2', 'idcliente2')
+      .leftJoinAndSelect('atendimento.idprodutolinha2', 'idprodutolinha2')
+      .leftJoinAndSelect('atendimento.idprodutoregional2', 'idprodutoregional2')
+      .leftJoinAndSelect('atendimento.idregional', 'idregional')
+      .leftJoinAndSelect('atendimento.idsolicitacaostatus', 'idsolicitacaostatus')
+      .leftJoinAndSelect('atendimento.unidade', 'unidade')
+      .leftJoinAndSelect('atendimento.previsaoreceitas', 'previsaoreceitas');
+
+    const result = await paginate<Atendimento>(query, queryBuilder, paginateConfig);
+
+    return result;
   }
 }
